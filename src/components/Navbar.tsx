29b3af1683f4d87d03dd30f9ee4d9072429ece7e
kalyan09122003 <kalyangk777@gmail.com>
2026-03-08 07:35:14 +0530

    Update latest changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          Logo
+          GK
         </a>
         <a
           href="mailto:kalyangk777@gmail.com"
